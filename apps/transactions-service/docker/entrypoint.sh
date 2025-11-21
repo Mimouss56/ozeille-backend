4da88c15a1c922849d522c0e09237706cc2e6c90
Kevin-HESSE <kev.hesse@gmail.com>
2025-11-21 12:12:46 +0100

    chore: add new services to docker
    (cherry picked from commit e1475dc0b6ee0eb6b17671b6d3ae872361a152c9)

---
+++ apps/transactions-service/docker/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "------------------------------------------"
+echo "  Démarrage du stage de développement NestJS "
+echo "------------------------------------------"
+
+# Vérifie si le dossier node_modules existe
+if [ ! -d "node_modules" ]; then
+  echo "Le dossier node_modules n'existe pas. Exécution de npm install..."
+  npm install
+  if [ $? -ne 0 ]; then
+    echo "ERREUR: pnpm install a échoué. Arrêt."
+    exit 1
+  fi
+  echo "npm install terminé."
+else
+  echo "Le dossier node_modules existe. Vérification des dépendances avec npm install..."
+  npm install || true # Utilise || true pour éviter l'échec si des warnings mineurs surviennent
+  if [ $? -ne 0 ]; then
+    echo "AVERTISSEMENT: npm install a rencontré des problèmes. Le serveur pourrait ne pas démarrer correctement."
+  fi
+  echo "Vérification des dépendances terminée."
+fi
+
+echo "Lancement du serveur NestJS en mode développement..."
+# Exécute la commande de démarrage de NestJS en mode dev
+# Le 'exec' est crucial pour s'assurer que les signaux d'arrêt sont transmis correctement au processus NestJS.
+exec npm run start:dev
+
+echo "------------------------------------------"
+echo "  Arrêt du stage de développement NestJS "
+echo "------------------------------------------"
