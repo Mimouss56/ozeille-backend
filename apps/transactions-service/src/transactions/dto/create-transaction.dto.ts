360549d4b2d31a554ab76d1a11540c3ba554e4bf
Kevin-HESSE <kev.hesse@gmail.com>
2025-12-08 17:24:25 +0100

    refacto: update createTransactionSchema

--- apps/transactions-service/src/transactions/dto/create-transaction.dto.ts
+++ apps/transactions-service/src/transactions/dto/create-transaction.dto.ts
@@ -1,13 +1,14 @@
+import { createZodDto } from "nestjs-zod";
 import { z } from "zod";
 
-export const createTransactionSchema = z
-  .object({
-    amount: z.float32().refine((val) => val !== 0, {
-      error: "amount must not be equal to 0",
-    }),
-    label: z.string().max(30),
-    dueAt: z.iso.datetime(),
-  })
-  .required();
+import { TransactionSchema } from "./transaction.dto";
+
+export const createTransactionSchema = TransactionSchema.pick({
+  amount: true,
+  label: true,
+  dueAt: true,
+}).required();
 
 export type CreateTransactionDto = z.infer<typeof createTransactionSchema>;
+
+export class CreateTransactionRequest extends createZodDto(createTransactionSchema) {}
