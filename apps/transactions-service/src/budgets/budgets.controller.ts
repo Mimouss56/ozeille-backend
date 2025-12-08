91e29a3e1d0e0b8cb9d9802393723fed59fa6185
Youva LOUNAS <youva.lounas.97@gmail.com>
2025-12-08 14:25:59 +0100

    feat(transactions-service) : add create budget in controller

--- apps/transactions-service/src/budgets/budgets.controller.ts
+++ apps/transactions-service/src/budgets/budgets.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post, UsePipes } from "@nestjs/common";
 import { BudgetsService } from "./budgets.service";
 import { Budget } from "./entities/budget.entity";
+import { ZodValidationPipe } from "src/pipe/ZodValidationPipe";
+import { CreateBudgetDto, createBudgetSchema } from "./dto/create-budget.dto";
 
 @Controller("budgets")
 export class BudgetsController {
@@ -9,5 +11,11 @@ export class BudgetsController {
   @Get("/")
   async findAll(): Promise<Budget[]> {
     return this.budgetsService.findAll();
+  }
+
+  @Post()
+  @UsePipes(ZodValidationPipe)
+  async create(@Body() createBudgetDto: CreateBudgetDto): Promise<Budget> {
+    return this.budgetsService.create(createBudgetDto);
   }
 }
