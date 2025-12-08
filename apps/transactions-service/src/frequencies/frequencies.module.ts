6aa239edb3b0ef803089f6ac1ca65ba4bf544b58
Thomas <123388670+BRC-Thomas@users.noreply.github.com>
2025-12-08 14:48:04 +0100

    feat/39- Add missing providers

--- apps/transactions-service/src/frequencies/frequencies.module.ts
+++ apps/transactions-service/src/frequencies/frequencies.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FrequenciesService } from './frequencies.service';
 import { FrequenciesController } from './frequencies.controller';
+import { FrequenciesRepository } from './repository/frequencies.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [FrequenciesController],
-  providers: [FrequenciesService],
+  providers: [FrequenciesService, FrequenciesRepository, PrismaService],
 })
 export class FrequenciesModule {}
