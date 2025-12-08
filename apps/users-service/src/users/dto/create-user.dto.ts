02d87258c6ee7ac3b27080ad7302adefe953ca40
Mimouss56 <lepriol.matthieu@gmail.com>
2025-12-08 14:55:20 +0100

    feat(users-service): ajouter la validation des utilisateurs avec nestjs-zod

--- apps/users-service/src/users/dto/create-user.dto.ts
+++ apps/users-service/src/users/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
+import { createZodDto } from "nestjs-zod";
 import { z } from "zod";
 
-export const createUserSchema = z.object({
-  email: z.string().email().max(50),
+export const CredentialsSchema = z.object({
+  email: z.email().max(50),
   password: z.string().min(8).max(255),
+  confirmedPassword: z.string().min(8).max(255),
   firstName: z.string().min(1).max(30),
   lastName: z.string().min(1).max(30),
 });
 
-export type CreateUserDto = z.infer<typeof createUserSchema>;
+export class CreateUserDto extends createZodDto(CredentialsSchema) {}
