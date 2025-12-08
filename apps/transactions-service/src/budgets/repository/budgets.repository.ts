c2aa34fdd4ae7526b33f260e633f41826f47d7e6
Youva LOUNAS <youva.lounas.97@gmail.com>
2025-12-08 14:25:48 +0100

    feat(transactions-service) : add create budget in repository

--- apps/transactions-service/src/budgets/repository/budgets.repository.ts
+++ apps/transactions-service/src/budgets/repository/budgets.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { Budget } from "../../generated/prisma/client";
 import { PrismaService } from "../../prisma/prisma.service";
+import { CreateBudgetDto } from "../dto/create-budget.dto";
 
 @Injectable()
 export class BudgetsRepository {
@@ -8,5 +9,9 @@ export class BudgetsRepository {
 
   async getAll(): Promise<Budget[]> {
     return this.prisma.budget.findMany();
+  }
+
+  async create(budget: CreateBudgetDto): Promise<Budget> {
+    return this.prisma.budget.create({ data: budget });
   }
 }
