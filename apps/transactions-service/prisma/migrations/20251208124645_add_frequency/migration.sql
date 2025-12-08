9708d0703b71c1a3cec206cfb4d89f2d08fadece
Thomas <123388670+BRC-Thomas@users.noreply.github.com>
2025-12-08 13:51:10 +0100

    feat/26-frequency-schema

---
+++ apps/transactions-service/prisma/migrations/20251208124645_add_frequency/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "transactions" ADD COLUMN     "frequency_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "frequencies" (
+    "id" TEXT NOT NULL,
+    "label" VARCHAR NOT NULL,
+    "monthly_value" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "frequencies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "frequencies_monthly_value_key" ON "frequencies"("monthly_value");
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_frequency_id_fkey" FOREIGN KEY ("frequency_id") REFERENCES "frequencies"("id") ON DELETE SET NULL ON UPDATE CASCADE;
