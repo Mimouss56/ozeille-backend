721fc19a423753734671fa7fbdb06ea69d9431c7
Youva LOUNAS <youva.lounas.97@gmail.com>
2025-12-09 13:17:01 +0100

    feat(transactions-service) :  add common fields.schema.ts

--- apps/transactions-service/src/budgets/dto/budget.dto.ts
+++ apps/transactions-service/src/budgets/dto/budget.dto.ts
@@ -1,18 +1,11 @@
 import { createZodDto } from "nestjs-zod";
+import { hexColorSchema } from "src/common/schemas/fields.schema";
 import { z } from "zod";
 
 export const budgetSchema = z.object({
   id: z.uuid(),
   label: z.string("Label must be a string").min(1).max(30).describe("The label of the budget"),
-  // Le regex pour les couleurs Hex, par exemple #FFF ou #FFFFFF
-  color: z
-    .string("Color must be a string")
-    .regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, {
-      message: "Color must be a valid Hex code (ex : #FFFFFF or #FFF)",
-    })
-    .nullable()
-    .optional()
-    .describe("The color of the budget in Hex format"),
+  color: hexColorSchema.nullable().optional().describe("The color of the budget in Hex format"),
 });
 
 export type BudgetDto = z.infer<typeof budgetSchema>;
