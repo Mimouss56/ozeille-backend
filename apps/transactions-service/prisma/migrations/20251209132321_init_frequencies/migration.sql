0d195455660f1091bd44ce17421caa8986bff415
Thomas <123388670+BRC-Thomas@users.noreply.github.com>
2025-12-09 14:24:32 +0100

    update migration frequencies

--- apps/transactions-service/prisma/migrations/20251209132321_init_frequencies/migration.sql
+++ apps/transactions-service/prisma/migrations/20251209132321_init_frequencies/migration.sql
@@ -5,13 +5,13 @@ ALTER TABLE "transactions" ADD COLUMN     "frequency_id" TEXT;
 CREATE TABLE "frequencies" (
     "id" TEXT NOT NULL,
     "label" VARCHAR NOT NULL,
-    "monthlyValue" DECIMAL(5,2) NOT NULL,
+    "monthly_value" DECIMAL(5,2) NOT NULL,
 
     CONSTRAINT "frequencies_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "frequencies_monthlyValue_key" ON "frequencies"("monthlyValue");
+CREATE UNIQUE INDEX "frequencies_monthly_value_key" ON "frequencies"("monthly_value");
 
 -- AddForeignKey
 ALTER TABLE "transactions" ADD CONSTRAINT "transactions_frequency_id_fkey" FOREIGN KEY ("frequency_id") REFERENCES "frequencies"("id") ON DELETE SET NULL ON UPDATE CASCADE;
