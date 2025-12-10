6d98921b8144ac26cf57a35712a2d90e50ae41f3
Mimouss56 <lepriol.matthieu@gmail.com>
2025-12-10 12:28:59 +0100

    feat: mettre à jour la configuration de Jest pour les tests TI et E2E et réorganiser l'importation de Redis

--- apps/users-service/src/mailer/mailer.module.ts
+++ apps/users-service/src/mailer/mailer.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import Redis from "ioredis";
 import { PrismaService } from "src/prisma/prisma.service";
 
 import { AuthUsecaseVerifyConfirmation } from "../auth/usecases/auth.usecase.verify-confirmation";
 import { MailerAlreadyExistUsecase } from "./usecase/mailer.usecase.already-exist";
 import { MailerUsecaseConfirmEmail } from "./usecase/mailer.usecase.confirm-email";
 import { MailerUsecaseSendMail } from "./usecase/mailer.usecase.send-mail";
-import Redis from "ioredis";
 
 @Module({
   imports: [ConfigModule],
