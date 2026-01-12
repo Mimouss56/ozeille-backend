3cc49f774c5a0d4423083e636e4e285815112470
Kevin-HESSE <kev.hesse@gmail.com>
2026-01-12 13:06:41 +0100

    fix(docker): Add dotenv to npm install

--- docker/entrypoint-prod.sh
+++ docker/entrypoint-prod.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Installing dependencies"
 cd /home/node/db
-npm i -D typescript@5.7.3 prisma@7.0.0
+npm i -D typescript@5.7.3 prisma@7.0.0 dotenv
 npm i @prisma/adapter-pg pg
 
 echo "Executing migrations"
