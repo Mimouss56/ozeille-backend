a9bec23edcc7933ff55ecda24514c51e6f8c5918
Kevin-HESSE <kev.hesse@gmail.com>
2026-01-12 13:51:34 +0100

    fix(docker): move install instruction for dockerfile

--- docker/entrypoint-prod.sh
+++ docker/entrypoint-prod.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-echo "Installing dependencies"
+echo "Moving to db directory"
 cd /home/node/db
-npm i -D typescript@5.7.3 prisma@7.0.0 dotenv
-npm i @prisma/adapter-pg pg
 
 echo "Executing migrations"
 npx prisma migrate deploy
