e71e2a544538c268f8bc6406670c3621a521b21b
Thomas <123388670+BRC-Thomas@users.noreply.github.com>
2026-02-03 14:37:38 +0100

    taille password a 12

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 
 export const UpdateUserSchema = z.object({
   email: z.email().max(50).optional(),
-  password: z.string().min(8).max(255).optional(),
-  confirmedPassword: z.string().min(8).max(255).optional(),
+  password: z.string().min(12).max(255).optional(),
+  confirmedPassword: z.string().min(12).max(255).optional(),
 });
 
 export class UpdateUserDto extends createZodDto(UpdateUserSchema) {}
