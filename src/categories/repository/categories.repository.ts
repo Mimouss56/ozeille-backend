d251b095d310a7cb03affb6bd4ee1d095596bdfe
Mimouss56 <lepriol.matthieu@gmail.com>
2026-02-03 22:42:28 +0100

    feat: définir limitAmount à 0 si null lors de la création d'une catégorie

--- src/categories/repository/categories.repository.ts
+++ src/categories/repository/categories.repository.ts
@@ -45,7 +45,7 @@ export class CategoriesRepository {
       data: {
         ...category,
         userId,
-        limitAmount: category.limitAmount === null ? undefined : category.limitAmount,
+        limitAmount: category.limitAmount === null ? 0 : category.limitAmount,
       },
     });
   }
