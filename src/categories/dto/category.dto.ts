ff326e8f5f71e6d6b6d39e68173a9c5d520bee3b
Mimouss56 <lepriol.matthieu@gmail.com>
2026-02-03 21:08:42 +0100

    feat: ajouter le type de transaction aux catégories avec ENUM et mise à jour des schémas DTO

--- src/categories/dto/category.dto.ts
+++ src/categories/dto/category.dto.ts
@@ -1,4 +1,5 @@
 import { createZodDto } from "nestjs-zod";
+import { TransactionType } from "src/generated/prisma/enums";
 import { z } from "zod";
 
 import { hexColorSchema } from "../../common/schemas/fields.schema";
@@ -9,10 +10,12 @@ export const categorySchema = z.object({
   label: z.string("Label must be a string").min(1).max(30).describe("The label of the category"),
   color: hexColorSchema.nullable().optional().describe("The color of the budget in Hex format"),
   userId: z.uuid().nullable().optional().describe("Owner ID (null for system categories)"),
+  type: z.enum(TransactionType).default(TransactionType.EXPENSE).describe("Transaction type"),
   limitAmount: z
     .number("Label must be a positive number")
     .min(0)
     .default(0)
+    .optional()
     .describe("Spending limit for this category"),
 });
 
